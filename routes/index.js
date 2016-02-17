220271d12e3b5a861f62f9d6c77361e8b8b94fdc
Ryan Weng <ryanweng24@gmail.com>
2016-02-17 18:30:05 +0000

    attempting coin flip fix

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,23 @@ var projects = require('../projects.json');
 /*
  * GET home page.
  */
-
+/*
 exports.view = function(req, res){
-  	projects["grid"] = false;
+	projects["grid"] = false;
   	res.render('index', projects);
+};
+*/
+exports.view = function(req, res){
+	var random_num = Math.random();
+	console.log(random_num);
+
+	if (random_num > 0.5) {
+		projects["grid"] = false;
+		res.render('index', projects);
+	} else {
+		console.log("else statement");
+		res.redirect('/grid');
+	}
 };
 
 exports.viewGrid = function(req, res){
